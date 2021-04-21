ac011465b570adfb94c1dbfcd874bf203d6eb682
coder.asher <grit.developer.asher@gmail.com>
2021-04-21 17:19:57 +0900

    Add new variable to fit length of all the summaries

--- src/Movie.js
+++ src/Movie.js
@@ -4,6 +4,9 @@ import { render } from 'react-dom';
 import './Movie.css';
 
 export default function Movie({ id, year, title, poster, summary, genres }) {
+  let cutSummary = summary.slice(0, 140);
+  cutSummary = cutSummary < 140 ? summary : cutSummary + '...';
+
   return (
     <div className='movie'>
       <img className='movie__poster' src={poster} alt={title} />
@@ -11,9 +14,13 @@ export default function Movie({ id, year, title, poster, summary, genres }) {
         <h3 className='movie__title'>{title}</h3>
         <h5 className='moive__year'>{year}</h5>
         <ul className='movie__genres'>
-          {genres.map((genre, index) => <li key={index} className='genres__genre'>{genre}</li>)}
+          {genres.map((genre, index) => (
+            <li key={index} className='genres__genre'>
+              {genre}
+            </li>
+          ))}
         </ul>
-        <p className='movie__summary'>{summary}</p>
+        <p className='movie__summary'>{cutSummary}</p>
       </div>
     </div>
   );
@@ -25,5 +32,5 @@ Movie.prototype = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
