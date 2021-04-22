3bdf06ad839b6376527611d8ef6e331efc000e93
Asher <grit.developer.asher@gmail.com>
2021-04-23 07:03:44 +0900

    Move main render page to Home.js and Home.cs

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    movie: [],
-  };
-
-  getMovie = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating',
-    );
-    this.setState({ movies: movies, isLoading: false }); //state: data
-  };
-
-  componentDidMount() {
-    this.getMovie();
-  }
-
+export default class App extends Component {
   render() {
-    const { isLoading, movies } = this.state;
     return (
-      <section className='container'>
-        {isLoading ? (
-          <div className='loader'>
-            <span className='loader__text'>Loading...</span>
-          </div>
-        ) : (
-          <div className='movies'>
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
+      <HashRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Route path='/' exact={true} component={Home} />
+        <Route path='/about' component={About} />
+      </HashRouter>
     );
   }
 }
-
-export default App;
