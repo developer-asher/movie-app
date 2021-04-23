539a937b8dce2233095ef246f16cacf4b7672789
coder.asher <grit.developer.asher@gmail.com>
2021-04-23 14:53:18 +0900

    Add navigation component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Navigation from './components/Navigation';
 
 export default class App extends Component {
   render() {
     return (
       <HashRouter>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+        <Navigation />
         <Route path='/' exact={true} component={Home} />
         <Route path='/about' component={About} />
       </HashRouter>
