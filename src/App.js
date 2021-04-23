4115d5731dfbb18328f9b086639c54cd3a877f76
coder.asher <grit.developer.asher@gmail.com>
2021-04-23 14:55:52 +0900

    Add movie detail component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
+import Detail from './routes/Detail';
 import Navigation from './components/Navigation';
 
 export default class App extends Component {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <Navigation />
         <Route path='/' exact={true} component={Home} />
         <Route path='/about' component={About} />
+        <Route path='/movie/:id' component={Detail} />
       </HashRouter>
     );
   }
